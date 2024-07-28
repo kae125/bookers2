ceea7c644bf8700710be972e0d38c28d14902131
k <ktf.ahmynket5317@gmail.com>
2024-07-28 12:35:12 +0000

    once finish

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,14 @@ Rails.application.routes.draw do
   devise_for :users
   
   resources :post_images, only: [:new, :index, :show, :edit]
-  resources :books, only: [:new, :index, :show, :edit]
+  resources :books, only: [:new, :index, :show, :edit, :destroy]
   resources :users, only: [:new, :index, :show, :edit]
   
   get 'homes/top'
   get 'homes/about' => 'homes#about', as: 'about'
   post 'books' => 'books#create'
   get 'books' => 'books#index'
+  patch 'books/:id' => 'books#update', as: 'update_book'
   
   root to: 'homes#top'
 
