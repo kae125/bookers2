9f278bc6951349b97d912b2a2c79e629961da2c5
k <ktf.ahmynket5317@gmail.com>
2024-08-11 11:53:40 +0000

    once finish

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   
   resources :post_images, only: [:new, :index, :show, :edit, :destroy]
   resources :books, only: [:new, :index, :show, :edit, :destroy]
-  resources :users, only: [:new, :index, :show, :edit]
+  resources :users, only: [:new, :index, :show, :edit, :update]
   
   get 'homes/top'
   get 'homes/about' => 'homes#about', as: 'about'
