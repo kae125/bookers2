ebe62081b327ce3b48688a215630b28e1f6ff17c
k <ktf.ahmynket5317@gmail.com>
2024-09-01 10:55:38 +0000

    user.rbのバリデーションなおした

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_08_23_083059) do
+ActiveRecord::Schema.define(version: 2024_08_04_104858) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -48,11 +48,6 @@ ActiveRecord::Schema.define(version: 2024_08_23_083059) do
     t.integer "user_id"
   end
 
-  create_table "postimages", force: :cascade do |t|
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -63,7 +58,6 @@ ActiveRecord::Schema.define(version: 2024_08_23_083059) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.text "introduction"
-    t.string "password_confirmation"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
