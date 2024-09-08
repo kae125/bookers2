47b86db867441a0c10ebac039af131cbf5db1d75
k <ktf.ahmynket5317@gmail.com>
2024-09-08 05:14:34 +0000

    bookのコントローラーのupdateflashにメッセージつけた

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -30,11 +30,13 @@ class BooksController < ApplicationController
 
   def edit
     @book = Book.find(params[:id])
+    
   end
   
   def update
     book = Book.find(params[:id])
     book.update(book_params)
+    flash[:notice] = "You have updated book successfully."
     redirect_to book_path(book.id) 
   end
   
